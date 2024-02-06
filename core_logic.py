13a49811a5be54d2e88fc286ca117947f7361c37
Qianchen Yang <130614574+Yorange1030@users.noreply.github.com>
2024-02-05 17:33:35 -0800

    Add files via upload

--- core_logic.py
+++ core_logic.py
@@ -1,1 +1,43 @@
 from vehicle import *
+
+def instruction_delivery_Emergency_Vehicle_At_1left(vehicle):
+    
+    if vehicle.vehicle_type == "normal_vehicle":
+
+        if vehicle.intersection_number == "1":
+            vehicle.action = "yield_left"
+
+        elif vehicle.intersection_number == "2" or vehicle.intersection_number == "3" or vehicle.intersection_number == "4":
+            vehicle.action = "stop"
+        
+    return vehicle.action
+
+def instruction_delivery_Emergency_Vehicle_At_1middle(vehicle):
+    
+    if vehicle.vehicle_type == "normal_vehicle":
+
+        if (vehicle.intersection_number == "2" and vehicle.lane_location == "right") or (vehicle.intersection_number == "3" and (vehicle.lane_location == "right" or vehicle.lane_location == "straight")):
+            vehicle.action = "continue"
+        elif (vehicle.intersection_number == "1") and (vehicle.lane_location == "left"):
+            vehicle.action = "yeild_left"
+        elif (vehicle.intersection_number == "1") and (vehicle.lane_location == "middle") or (vehicle.intersection_number == "1") and (vehicle.lane_location == "right"):
+            vehicle.action = "yeild_right"
+        else:
+            vehicle.action = "stop"
+        
+    return vehicle.action
+
+def instruction_delivery_Emergency_Vehicle_At_1right(vehicle):
+        
+    if vehicle.vehicle_type == "normal_vehicle":
+
+        if vehicle.intersection_number == "1":
+            vehicle.action = "yield_left"
+
+        elif vehicle.intersection_number == "2" and vehicle.lane_location == "middle":
+            vehicle.action = "stop"
+        
+        else:
+            vehicle.action = "continue"
+        
+    return vehicle.action
--- vehicle.py
+++ vehicle.py
@@ -1,8 +1,10 @@
 class Vehicle:
-    def __init__(self, vehicle_type, centroid_location, ID, intersection_direction, lane_location, action):
-        self.vehicle_type = vehicle_type  
-        self.centroid_location = centroid_location 
+    def __init__(self, vehicle_type, centroid_location, ID, intersection_number, lane_location, action):
+        self.vehicle_type = vehicle_type  # "normal_vehicle" or "emergency_vehicle"
+        self.centroid_location = centroid_location  # (x, y) of the vehicle's centroid
         self.ID = ID 
-        self.intersection_direction = intersection_direction  
-        self.lane_location = lane_location  
-        self.action = action  
+        self.intersection_number = intersection_number  # "1", "2", "3", "4"
+        self.lane_location = lane_location  # "left", "middle", "right"
+        self.action = action  # "stop", "yield_left", "yield_right", "continue"
+
+    
