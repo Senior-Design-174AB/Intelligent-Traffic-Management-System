8aff2a9ae7693cd162d27f9bd82cb34e414c9a2f
Qianchen Yang <130614574+Yorange1030@users.noreply.github.com>
2024-02-26 00:55:20 -0800

    Add files via upload

--- core_logic.py
+++ core_logic.py
@@ -1,40 +1,54 @@
 from vehicle import *
 
-def instruction_delivery_Emergency_Vehicle_At_1left(vehicle):
+def normal_vehicle_number_change(intersection_number):
+    if intersection_number == "1":
+        return ("1", "2", "3", "4")
+    if intersection_number == "2":
+        return ("2", "3", "4", "1")
+    if intersection_number == "3":
+        return ("3", "4", "1", "2")
+    if intersection_number == "4":
+        return ("4", "1", "2", "3")
+    pass
+
+def instruction_delivery_Emergency_Vehicle_At_left(vehicle,emergency_intersection_number):
+    section_1, section_2, section_3, section_4 = normal_vehicle_number_change(emergency_intersection_number)
     
     if vehicle.vehicle_type == "normal_vehicle":
 
-        if vehicle.intersection_number == "1":
+        if vehicle.intersection_number == section_1:
             vehicle.action = "yield_left"
 
-        elif vehicle.intersection_number == "2" or vehicle.intersection_number == "3" or vehicle.intersection_number == "4":
+        elif vehicle.intersection_number == section_2 or vehicle.intersection_number == section_3 or vehicle.intersection_number == section_4:
             vehicle.action = "stop"
         
     return vehicle.action
 
-def instruction_delivery_Emergency_Vehicle_At_1middle(vehicle):
+def instruction_delivery_Emergency_Vehicle_At_middle(vehicle, emergency_intersection_number):
+    section_1, section_2, section_3, section_4 = normal_vehicle_number_change(emergency_intersection_number)
     
     if vehicle.vehicle_type == "normal_vehicle":
 
-        if (vehicle.intersection_number == "2" and vehicle.lane_location == "right") or (vehicle.intersection_number == "3" and (vehicle.lane_location == "right" or vehicle.lane_location == "straight")):
+        if (vehicle.intersection_number == section_2 and vehicle.lane_location == "right") or (vehicle.intersection_number == section_3 and (vehicle.lane_location == "right" or vehicle.lane_location == "straight")):
             vehicle.action = "continue"
-        elif (vehicle.intersection_number == "1") and (vehicle.lane_location == "left"):
+        elif (vehicle.intersection_number == section_1) and (vehicle.lane_location == "left"):
             vehicle.action = "yeild_left"
-        elif (vehicle.intersection_number == "1") and (vehicle.lane_location == "middle") or (vehicle.intersection_number == "1") and (vehicle.lane_location == "right"):
+        elif (vehicle.intersection_number == section_1) and (vehicle.lane_location == "middle") or (vehicle.intersection_number == section_1) and (vehicle.lane_location == "right"):
             vehicle.action = "yeild_right"
         else:
             vehicle.action = "stop"
         
     return vehicle.action
 
-def instruction_delivery_Emergency_Vehicle_At_1right(vehicle):
+def instruction_delivery_Emergency_Vehicle_At_right(vehicle, emergency_intersection_number):
+    section_1, section_2, section_3, section_4 = normal_vehicle_number_change(emergency_intersection_number)
         
     if vehicle.vehicle_type == "normal_vehicle":
 
-        if vehicle.intersection_number == "1":
+        if vehicle.intersection_number == section_1:
             vehicle.action = "yield_left"
 
-        elif vehicle.intersection_number == "2" and vehicle.lane_location == "middle":
+        elif vehicle.intersection_number == section_2 and vehicle.lane_location == "middle":
             vehicle.action = "stop"
         
         else:
